c42e51a5e873870bbf82f53d7df2b3000c9ca2ce
Niraaxx <tanguycrussard@gmail.com>
2022-01-18 15:46:39 +0100

    Scrapping des prix en place, image à compléter

--- maquette/index.js
+++ maquette/index.js
@@ -8,7 +8,7 @@ const fetch = require('node-fetch')
 
 var app = express();
 
-const URL = "https://www.probikeshop.fr/bmx/bmx-street-dirt-cadres-c3258.html";
+const URLCadres = "https://www.probikeshop.fr/bmx/bmx-street-dirt-cadres-c3258.html";
 
 const server = require('http').createServer(app);
 
@@ -42,20 +42,19 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-
-//Récupère un élément depuis un lien et l'ajoute à la BD (pour l'instant: seulement image)
-async function scrapeProduct(url){
+//Récupère le prix des cadres depuis un lien
+async function scrapePrixCadres(url){
     const browser = await puppeteer.launch();
     const page = await browser.newPage()
     await page.goto(url)
 
+    //Scrapping des prix des cadres
     let prixTable = [];
     prixTable = await page.evaluate(() => {
         return Array.from(document.querySelectorAll(".product_price")).map(x => x.textContent);
     });
-    let regexPrix = /[0-9]{1-5},[0-9]{2}/;
     prixTable = prixTable.map(function (el) {
-        return el.trim().split(regexPrix);
+        return el.trim().split(/[0-9]{1-5},[0-9]{2}/);
     });
     for(let i =0; i <prixTable.length; i++){
         prixTable[i][0] = prixTable[i][0].replace(/,/g, ".");
@@ -70,7 +69,18 @@ async function scrapeProduct(url){
         prixTable[i] = parseFloat(prixTable[i]);
     }
 
-    console.log(prixTable);
+    await browser.close();
+
+    //console.log(prixTable); Pour vérifier que ça extrait bien
+
+    return prixTable;
+}
+
+//Récupère un élément depuis un lien et l'ajoute à la BD (pour l'instant: seulement image)
+async function scrapeProduct(url){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage()
+    await page.goto(url)
 
     let imgProduct = [];
     imgProduct = await page.evaluate(() => {
@@ -103,8 +113,6 @@ async function scrapeProduct(url){
     const src = await el.getProperty('src');
     const srcTxt = await src.jsonValue();
     console.log({srcTxt});*/
-
-    browser.close();
 
     let piece = {
         _id: 20,
@@ -159,4 +167,5 @@ app.get('/importDataPreset', async function(req, res) {
     });
   });
 
-//scrapeProduct(URL)
+scrapePrixCadres(URLCadres);
+//scrapeProduct(URLCadres);
