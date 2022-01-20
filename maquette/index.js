6073536d70d91ff8b1c42f964603015aab7a54f4
Niraaxx <tanguycrussard@gmail.com>
2022-01-20 20:56:47 +0100

    Début de scrapping du deuxième site
    A fixer plusieurs bug

--- maquette/index.js
+++ maquette/index.js
@@ -14,6 +14,13 @@ const URLGuidons = "https://www.probikeshop.fr/bmx/bmx-street-dirt-peripheriques
 const URLPlateaux = "https://www.probikeshop.fr/bmx/bmx-street-dirt-transmission-plateaux-c3297.html";
 const URLSelles = "https://www.probikeshop.fr/bmx/bmx-street-dirt-peripheriques-selles-c3274.html";
 
+const URLCadres2 ="https://www.alltricks.fr/C-41254-cadres?_gl=1*1qibc9t*_up*MQ..&gclid=Cj0KCQiAraSPBhDuARIsAM3Js4qVJ1tXvoEbQAaWKPbF69pAEMCetqMPuFkdOfIL7C_Cf-7RWaCuuZ4aAsvZEALw_wcB.html";
+const URLPneus2 = "https://www.alltricks.fr/C-41273-pneus.html";
+const URLGuidons2 = "https://www.alltricks.fr/C-41257-guidons.html";
+const URLPlateaux2 = "https://www.alltricks.fr/C-41263-plateaux";
+const URLSelles2 = "https://www.alltricks.fr/C-45868-selles.html";
+
+
 
 const server = require('http').createServer(app);
 
@@ -83,6 +90,86 @@ app.get('/importDataPreset', async function(req, res) {
 
 
 var index=0;    //ID pour les éléménts de la BD
+async function scrapeElements2(url, id_partie){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage()
+    await page.goto(url);
+
+    //Scrapping des prix
+    let prixTable = [];
+    prixTable = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(".alltricks-Product-actualPrice")).map(x => x.textContent);
+    });
+    prixTable = prixTable.map(function (el) {
+        return el.trim().split(/[0-9]{1-5},[0-9]{2}/);
+    });
+    for(let i =0; i <prixTable.length; i++){
+        prixTable[i][0] = prixTable[i][0].replace(/,/g, ".");
+        if(prixTable[i][0].includes('-')){
+            prixTable[i] = prixTable[i][0].substring(32,38);
+        }
+        if(prixTable[i][0].includes('€')){
+            prixTable[i] = prixTable[i][0].substring(0,6);
+        }
+    }
+    for(let i=0; i<prixTable.length; i++){
+        prixTable[i] = parseFloat(prixTable[i]);
+    }
+
+    console.log(prixTable);
+
+    //Scrapping des images
+    let imagesTable = [];
+    imagesTable = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(".alltricks-Product-picture")).map(x => x.children[0].getAttribute("src"));
+    });
+    console.log(imagesTable);
+
+    //Scrapping des noms
+    let nomsTable = [];
+    nomsTable = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(".alltricks-Product-description")).map(x => x.textContent.trim());
+    });
+    console.log(nomsTable);
+   
+    //Scrapping des liens
+    let liensTable = [];
+    liensTable = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(".alltricks-Product--grid")).map(x => ()=>{
+            if("https://www.alltricks.fr/" + x.children[3].getAttribute("href") == "https://www.alltricks.fr/null"){
+                return "https://www.alltricks.fr/" + x.children[2].getAttribute("href");
+            }else{
+                return "https://www.alltricks.fr/" + x.children[3].getAttribute("href");
+            }
+        })
+        let table2 =  Array.from(document.querySelectorAll(".alltricks-Product--grid")).map(x => "https://www.alltricks.fr/" + x.children[2].getAttribute("href"));
+        result = [];
+        for(let el1 of table){
+            let i = 0;
+            let find = false;
+            while(table2.length < i && find == false){
+                if(el1 == "https://www.alltricks.fr/null"){
+                    result[i] = el2;
+                    find = true;
+                }else{
+                    result[i] = el1;
+                    find = true; 
+                }
+            }
+        }
+        return result;
+    });
+    console.log(liensTable);
+
+    //Scrapping des marques 
+    let marquesTable = [];
+    marquesTable = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(".alltricks-Product-brandLabel")).map(x => x.textContent.trim());
+    });
+
+    console.log(marquesTable);
+    await browser.close();
+}
 
 async function scrapeElements(url, id_partie){
     const browser = await puppeteer.launch();
@@ -166,6 +253,11 @@ async function scrapAll(){
     scrapeElements(URLGuidons,3);
     scrapeElements(URLPlateaux,4);
     scrapeElements(URLSelles,5);
+    //scrapeElements2(URLCadres2,1);
+    //scrapeElements2(URLPneus2,2);
+    scrapeElements2(URLGuidons2,3);
+    //scrapeElements2(URLPlateaux2,4);
+    //scrapeElements2(URLSelles2,5);
 }
 
-scrapAll()
+scrapAll();
